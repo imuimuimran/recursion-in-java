2abfa52635be59592db1ba0bb24bff298c720c2b
Mohd. Imran Hossain <hossainmohdimran@gmail.com>
2024-02-06 11:19:43 +0600

    This is basic syntax of recursion program in java.

--- basicRecursion_02.java
+++ basicRecursion_02.java
@@ -1,18 +1,18 @@
 import java.util.*;
 
-public class basicRecursion_01 {
+public class basicRecursion_02 {
     public static void printNum(int num) {
-        if(num == 0) {
+        if(num == 6) {
             return;
         }
 
         System.out.println(num);
-        printNum(num - 1);
+        printNum(num + 1);
     }
 
     public static void main(String arg[]) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a number whice you want to recursion with decreasing: ");
+        System.out.print("Enter a number whice you want to recursion with increasing: ");
         int num = sc.nextInt();
 
         printNum(num);
