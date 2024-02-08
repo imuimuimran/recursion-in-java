c6495e1968e0398a365367cee60232dd1e3e932b
Mohd. Imran Hossain <hossainmohdimran@gmail.com>
2024-02-08 21:40:36 +0600

    This program can print string in reverse way in recursive method.

---
+++ printStringinReverse.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class printStringinReverse {
+        public static String revString(String str) {
+                if(str.length() == 1) {
+                return str;
+            }
+            char currChar = str.charAt(0);
+            String nextString = revString(str.substring(1));
+            return nextString + currChar;
+        }
+        public static void main(String args[]) {
+            Scanner sc= new Scanner(System.in);
+            System.out.print("Enter Your Name with multiple word: ");  
+            String str = sc.nextLine(); 
+
+            String reversed = revString(str);
+            System.out.println(reversed);
+
+            sc.close();
+        }
+    }
