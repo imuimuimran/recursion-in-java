330de4047f91ee6e31f07694fb651071909ed339
Mohd. Imran Hossain <hossainmohdimran@gmail.com>
2024-02-09 13:43:39 +0600

    Modify The program which can print string with recursive method.

--- printString.java
+++ printString.java
@@ -1,16 +1,16 @@
 import java.util.*;
 public class printString {
-        public static String revString(String str) {
+        public static String Strings(String str) {
                 if(str.length() == 1) {
                 return str;
             }
             char currChar = str.charAt(0);
-            String nextString = revString(str.substring(1));
+            String nextString = Strings(str.substring(1));
             return nextString + currChar;
         }
         public static void main(String args[]) {
             Scanner sc= new Scanner(System.in);
-            System.out.print("Enter Your Name with multiple word: ");  
+            System.out.print("Enter a word or sentence: ");  
             String str = sc.nextLine(); 
 
             System.out.println(str);
