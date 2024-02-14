96cdb09e9ecaf83c5a9c19ae9124a3077b82269d
Mohd. Imran Hossain <hossainmohdimran@gmail.com>
2024-02-14 15:44:53 +0600

    This updated program can print keypad combination of subsequence of string.

--- keypadCombination_02.java
+++ keypadCombination_02.java
@@ -1,4 +1,19 @@
 public class keypadCombination_02 {
+
+    /*
+        0 -> .;
+        1 -> abc
+        2 -> def
+        3 -> ghi
+        4 -> jkl
+        5 -> mno
+        6 -> pqrs
+        7 -> tu
+        8 -> vwx
+        9 -> yz
+     */
+
+
     public static String keypad[] = {".", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tu", "vwx", "yz"};
     public static void printKeypadCombination(String number, int idx, String res) {
         if(idx == number.length()) {
