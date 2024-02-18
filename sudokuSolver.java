bedfe2a4b87a3b79099f52814352927811e65373
Mohd. Imran Hossain <hossainmohdimran@gmail.com>
2024-02-18 15:01:11 +0600

    This is primary structure to solve sudoku problem in recursive method.

---
+++ sudokuSolver.java
@@ -0,0 +1,36 @@
+public class sudokuSolver {
+    public static void main(String[] args) {
+        // Input Grid 9 x 9
+        int[][] grid = new int[][] {
+            {5, 3, 0, 0, 7, 0, 0, 0, 0},
+            {6, 0, 0, 1, 9, 5, 0, 0, 0},
+            {0, 9, 8, 0, 0, 0, 0, 6, 0},
+            {8, 0, 0, 0, 6, 0, 0, 0, 0},
+            {4, 0, 0, 8, 0, 3, 0, 0, 1},
+            {7, 0, 0, 0, 2, 0, 0, 0, 6},
+            {0, 6, 0, 0, 0, 0, 2, 8, 0},
+            {0, 0, 0, 4, 1, 9, 0, 0, 0},
+            {0, 0, 0, 0, 8, 0, 0, 7, 9}
+        };
+        // Creating an object
+        sudokuSolver obj = new sudokuSolver();
+
+        // Calculate the size of the grid
+        int size = grid.length;
+        System.out.println("The grid is: ");
+        for(int i = 0; i < size; i++) {
+            for(int j = 0; j < size; j++) {
+                System.out.print(grid[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+        if(obj.solveSudoku(grid, size)) {
+            // Print the solution
+            System.out.println("The Solution of given Sudoku is: ");
+            obj.display(grid, size);
+        }else {
+            System.out.println("The solution is not possible!");
+        }
+    }
+}
